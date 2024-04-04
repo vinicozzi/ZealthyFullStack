b288fb73bb4e8c9a1af5f335a5c2a462de686c3e
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-04-04 13:37:45 -0400

    test 2 zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+// import Head from "next/head";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="user-scalable=no width=device-width, initial-scale=1, maximum-scale=1" />
-      </Head>
+      
+      <meta name="viewport" content="user-scalable=no width=device-width, initial-scale=1, maximum-scale=1" />
       <body className={inter.className}>{children}</body>
     </html>
   );
